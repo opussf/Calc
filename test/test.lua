e03728b82654ed658f022a74dee070e5ccba70f7
opussf <github@zz9-za.com>
2015-11-11 14:55:11 -0800

    3 new tests

--- test/test.lua
+++ test/test.lua
@@ -2,3 +2,14 @@ I'm working on master now.
 
 This file will be changed in both branches.
 And I will rebase working off of the new master commit.
+
+function test.test_MSG_ADDONNAME()
+	assertEquals( "Calc", CALC_MSG_ADDONNAME )
+end
+function test.test_MSG_VERSION()
+	-- just make sure this is assigned
+	assert( CALC_MSG_VERSION )
+end
+function test.test_MSG_AUTHOR()
+	assert( CALC_MSG_AUTHOR )
+end
