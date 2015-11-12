e74dd34b2cdca8bf4fe2be89280584a9d083245c
opussf <github@zz9-za.com>
2015-11-11 23:11:01 -0800

    More Tests

--- test/test.lua
+++ test/test.lua
@@ -12,6 +12,10 @@ test.outFileName = "testOut.xml"
 package.path = "../src/?.lua;'" .. package.path
 require "calc"
 
+function test.before()
+	calc.stack = {}
+end
+
 function test.test_MSG_ADDONNAME()
 	assert( CALC_MSG_ADDONNAME )
 end
@@ -28,4 +32,20 @@ end
 function test.test_Command()
 	calc.Command()
 end
+function test.test_PutOnStack()
+	calc.Command( "5" )
+	assertEquals( 5, calc.stack[1] )
+end
+function test.test_PutTwoOnStack()
+	calc.Command( "5" )
+	calc.Command( "7" )
+	assertEquals( 7, calc.stack[2] )
+end
+function test.test_PutTwoOnStack_oneLine()
+	calc.Command( "5 7")
+	assertEquals( 5, calc.stack[1] )
+	assertEquals( 7, calc.stack[2] )
+end
+
+
 test.run()
