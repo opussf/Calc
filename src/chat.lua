aa56064c1bd8f2cc8fca947e96e4802c7cb80127
opussf <github@zz9-za.com>
2018-05-11 14:15:47 -0700

    Intercept chat messages with "==", and parse them for values and commands.  Replace "==" with "= stack".

---
+++ src/chat.lua
@@ -0,0 +1,32 @@
+function calc.SendChatMessage( msgIn, system, language, channel )
+	--print( "calc.SendChatMessage( '"..msgIn.."', "..system..", "..( language or "nil" )..", "..( channel or "nil" ).." )" )
+	msgNew = nil
+	local hasEquals = strfind( msgIn, "==" )
+	if( hasEquals ) then
+		msg = string.lower( msgIn )
+		while msg and string.len( msg ) > 0 do
+			val, msg = calc.Parse( msg )
+			if val then
+				if calc.functions[val] then
+					calc.functions[val]()
+				elseif tonumber(val) then
+					table.insert( calc.stack, tonumber(val) )
+				end
+			end
+		end
+		local result = table.concat( calc.stack, " " )
+		msgNew = string.gsub( msgIn, "==", "= "..result )
+		--print( "New msg: "..msgNew )
+	end
+	--print( "msg out: "..( msgNew or msgIn ) )
+	calc.OriginalSendChatMessage( ( msgNew or msgIn ), system, language, channel )
+end
+function calc.OnLoad()
+	CALC_Frame:RegisterEvent( "VARIABLES_LOADED" )
+end
+function calc.VARIABLES_LOADED()
+	CALC_Frame:UnregisterEvent( "VARIABLES_LOADED" )
+	-- Intercept chat events
+	calc.OriginalSendChatMessage = SendChatMessage
+	SendChatMessage = calc.SendChatMessage
+end
