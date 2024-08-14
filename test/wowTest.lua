4bccece4e4be89d70a2454fa2b28b7bd975102c0
opussf <github@zz9-za.com>
2024-08-13 22:10:29 -0700

    110002 Updates
    The trouble with not having a goal is that you can spend your life running up and down the field and never score.

--- test/wowTest.lua
+++ test/wowTest.lua
@@ -1,7 +1,7 @@
 -----------------------------------------
 -- Author  :  Opussf
--- Date    :  August 6 2024
--- Revision:  9.4.3-11-ga7a1da8
+-- Date    :  August 13 2024
+-- Revision:  9.4.3-17-g11678c7
 -----------------------------------------
 -- This is an uber simple unit test implementation
 -- It creates a dictionary called test.
